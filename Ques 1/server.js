e47d07f1c7e9a459f7a14fc5cefcd4cd8ba11c9d
Srajan Bansal <srajanbansal999@gmail.com>
2024-07-12 16:55:00 +0530

    Ques 1 photos and Ques 2

--- Ques 1/server.js
+++ Ques 1/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
 const productRoutes = require('./routes/product.route');
+
+app.use(
+	cors({
+		origin: 'http://localhost5173',
+	})
+);
 
 app.use('/categories', productRoutes);
 
